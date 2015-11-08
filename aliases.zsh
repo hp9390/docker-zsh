aacc93d5198123b134a4082b87a82fd69ba8a5ce
Holger Piontek <hp9390@gmail.com>
2015-11-08 18:32:39 +0100

    Remove ntpdate.

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,3 @@
 function gitignore() { curl -L -s https://www.gitignore.io/api/\$@ ; }
-alias updateTime='ntpdate -u pool.ntp.org 24.56.178.140 2607:f248::45'
-alias upd='updateTime && apt-get autoremove -y && apt-get clean && apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y'
+alias upd='apt-get autoremove -y && apt-get clean && apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y'
 alias add=' apt-get install -y'
